38a58f197f77c403724e8c2f14d12aa0c9e6aa6f
Arjan Einbu <arjan@einbu.no>
2018-10-19 23:37:21 +0200

    Tests for trad/toggleValueInArray

--- test/toggleTradTests.js
+++ test/toggleTradTests.js
@@ -1,16 +1,15 @@
-const toggleValueInArray = require("../source/fp/toggleValueInArray");
+const toggleValueInArray = require("../source/trad/toggleValueInArray");
 
 const { describe, it, expect} = require("mocha");
 const context = describe;
 const { assert }  = require("chai");
 
 
-describe("fp/toggleValueInArray", function(){
+describe("trad/toggleValueInArray", function(){
 	context("empty array", function(){
 		it("should have one element after toggle", function(){
 			let arr = [];
-			let toggler = toggleValueInArray(arr);
-			toggler("shirt");
+			toggleValueInArray("shirt", arr);
 	
 			assert.sameMembers(arr, ["shirt"]);
 		});	
@@ -18,15 +17,13 @@ describe("fp/toggleValueInArray", function(){
 	context("array containing one element", function(){
 		it("should have two elements after toggle something not in array", function(){
 			let arr = ["pants"];
-			let toggler = toggleValueInArray(arr);
-			toggler("shirt");
+			toggleValueInArray("shirt", arr);
 	
 			assert.sameMembers(arr, ["pants", "shirt"]);
 		});	
 		it("should have no elements after toggle something in array", function(){
 			let arr = ["pants"];
-			let toggler = toggleValueInArray(arr);
-			toggler("pants");
+			toggleValueInArray("pants", arr);
 	
 			assert.sameMembers(arr, []);
 		});	
@@ -34,15 +31,13 @@ describe("fp/toggleValueInArray", function(){
 	context("starts with array containing many elements", function(){
 		it("should add element when toggle something not in array", function(){
 			let arr = ["pants", "socks"];
-			let toggler = toggleValueInArray(arr);
-			toggler("shirt");
+			toggleValueInArray("shirt", arr);
 	
 			assert.sameMembers(arr, ["pants", "socks", "shirt"]);
 		});	
 		it("should remove element when toggle something in array", function(){
 			let arr = ["pants", "socks"];
-			let toggler = toggleValueInArray(arr);
-			toggler("pants");
+			toggleValueInArray("pants", arr);
 	
 			assert.sameMembers(arr, ["socks"]);
 		});	
